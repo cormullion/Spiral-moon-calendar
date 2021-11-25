feb5b5e964bdfb698930a21d92e2bbfa1a0a22bc
cormullion <cormullion@mac.com>
2021-11-25 15:54:12 +0000

    update 2022

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 Spiral moon phase calendar
 ====================
 
-A spiral moon phase calendar, using Luxor.jlto draw the graphics and calculate the phases.
+A spiral moon phase calendar, using Luxor.jl to draw the graphics and calculate the phases.
 
-I've switched to using data downloaded from NASA - the Astrolib calculations are slightly off now (I expect there are some out of data constants, or perhaps the solar system has been shifting recently...).
-
-<img src="2021-moon-phase-calendar.png" width = 800>
+<img src="2022-moon-phase-calendar.png" width = 800>
--- spiral-moon-phase-calendar.jl
+++ spiral-moon-phase-calendar.jl
@@ -1,77 +1,79 @@
-using Colors, Luxor, Dates, JSON3, DataFrames
+using Colors, Luxor, Dates, JSON3, DataFrames, Downloads
 
-# read data from a dwonloaded copy https://svs.gsfc.nasa.gov/vis/a000000/a004800/a004874/mooninfo_2021.json
-# saved as "nasa-moon-2021-data.json"
+#=
+download moon data file from NASA, draw a spiral calendar
+=#
 
-function lefthemi(pt, r, col)         # draw a left hemisphere
-    gsave()
-    sethue(col)
-    newpath()
-    arc(pt, r, pi / 2, -pi / 2, :fill)    # positive clockwise from x axis in radians
-    grestore()
+function lefthemi(pt, r, col)             # draw a left hemisphere
+    @layer begin
+        sethue(col)
+        newpath()
+        arc(pt, r, pi / 2, -pi / 2, :fill)    # positive clockwise from x axis in radians
+    end
 end
 
 function righthemi(pt, r, col)
-    gsave()
-    sethue(col)
-    newpath()
-    arc(pt, r, -pi / 2, pi / 2, :fill)    # positive clockwise from x axis in radians
-    grestore()
+    @layer begin
+        sethue(col)
+        newpath()
+        arc(pt, r, -pi / 2, pi / 2, :fill)    # positive clockwise from x axis in radians
+    end
 end
 
 function elliptical(pt, r, col, horizscale)
-    gsave()
-    sethue(col)
-    Luxor.scale(horizscale + 0.00000001, 1) # cairo doesn't like 0 scale :)
-    circle(pt, r, :fill)
-    grestore()
+    @layer begin
+        sethue(col)
+        Luxor.scale(horizscale + 0.00000001, 1) # cairo doesn't like 0 scale :)
+        circle(pt, r, :fill)
+    end
 end
 
 function getdata()
-    json_string = read("nasa-moon-2021-data.json", String)
+    json_string = read("nasa-moon-2022-data.json", String)
     data = JSON3.read(json_string)
     df = DataFrame(data)
     df.isotime = map(dt -> DateTime(replace(dt, r" UT$" => ""), "d u y HH:MM"), df.time)
     return df
 end
 
-function moon(pt::Point, r, age, positionangle,
-        foregroundcolour = "darkblue",
-        backgroundcolour = "gray")
-    # draw a moon by superimposing three circles or ellipticals
-    # phase is moon's age normalized to 0 - 1, 0 = new moon, 0.5 = 14 days/full, 1.0 = dead moon
+# draw a moon by superimposing three circles or ellipticals
+
+function moon(pt::Point, r, age, positionangle)
+    # use moon synodic period
+    age = rescale(age, 0, 29.53059)
     # elliptical is scaled horizontally to render phases
-    gsave()
-    translate(pt)
-    setopacity(1)
-    if 0 <= age < 0.25
-        righthemi(pt, r, foregroundcolour)
-        moonwidth = 1 - (age * 4) # goes from 1 down to 0 width (for half moon)
-        setopacity(0.5)
-        elliptical(O, r + 0.02, RGB(25 / 255, 25 / 255, 100 / 255), moonwidth + 0.02)
-        setopacity(1.0)
-        elliptical(O, r, backgroundcolour, moonwidth)
-        lefthemi(O, r, backgroundcolour)
-    elseif 0.25 <= age < 0.50
-        lefthemi(O, r, backgroundcolour)
-        righthemi(O, r, foregroundcolour)
-        moonwidth = (age - .25) * 4
-        elliptical(O, r, foregroundcolour, moonwidth)
-    elseif .50 <= age < .75
-        lefthemi(O, r, foregroundcolour)
-        righthemi(O, r, backgroundcolour)
-        moonwidth = 1 - ((age - 0.5) * 4)
-        elliptical(O, r, foregroundcolour, moonwidth)
-    elseif .75 <= age <= 1.00
-        lefthemi(O, r, foregroundcolour)
-        moonwidth = ((age - 0.75) * 4)
-        setopacity(0.5)
-        elliptical(O, r + 0.02, RGB(25 / 255, 25 / 255, 100 / 255), moonwidth + 0.02)
-        setopacity(1.0)
-        elliptical(O, r, backgroundcolour, moonwidth)
-        righthemi(O, r, backgroundcolour)
+    @layer begin
+        translate(pt)
+        rotate(deg2rad(positionangle/30)) # reduce apparent rotation for aesthetic
+        setopacity(1)
+        if 0 <= age < 0.25
+            righthemi(pt, r, foregroundwhite)
+            moonwidth = 1 - (age * 4) # goes from 1 down to 0 width (for half moon)
+            setopacity(0.5)
+            elliptical(O, r + 0.02, RGB(25 / 255, 25 / 255, 100 / 255), moonwidth + 0.02)
+            setopacity(1.0)
+            elliptical(O, r, backgroundblue, moonwidth)
+            lefthemi(O, r, backgroundblue)
+        elseif 0.25 <= age < 0.50
+            lefthemi(O, r, backgroundblue)
+            righthemi(O, r, foregroundwhite)
+            moonwidth = (age - .25) * 4
+            elliptical(O, r, foregroundwhite, moonwidth)
+        elseif .50 <= age < .75
+            lefthemi(O, r, foregroundwhite)
+            righthemi(O, r, backgroundblue)
+            moonwidth = 1 - ((age - 0.5) * 4)
+            elliptical(O, r, foregroundwhite, moonwidth)
+        elseif .75 <= age <= 1.00
+            lefthemi(O, r, foregroundwhite)
+            moonwidth = ((age - 0.75) * 4)
+            setopacity(0.5)
+            elliptical(O, r + 0.02, RGB(25 / 255, 25 / 255, 100 / 255), moonwidth + 0.02)
+            setopacity(1.0)
+            elliptical(O, r, backgroundblue, moonwidth)
+            righthemi(O, r, backgroundblue)
+        end
     end
-    grestore()
 end
 
 function spiral_calendar(theyear, currentwidth, currentheight)
@@ -79,14 +81,15 @@ function spiral_calendar(theyear, currentwidth, currentheight)
     x = y = 0.0
     centerX = centerY = 0
     radius = 50.0 # starting radius
-    moonsize = 16
-    rotation = pi
+    moonsize = 16 # pixels
+    rotation = π
     away = 0
+    # spiral stuff
     # How far to step away from center.
     awayStep = moonsize * 0.9
     chord = moonsize * 2.5 # distance between centers (should be more than twice the moon radius...)
     theta = chord / awayStep
-    sethue("yellow")
+    sethue(foregroundwhite)
 
     daterange = collect(Date(currentyear, 12, 31):-Dates.Day(1):Date(currentyear, 1, 1))
     df = getdata()
@@ -99,46 +102,50 @@ function spiral_calendar(theyear, currentwidth, currentheight)
         x = centerX + (cos(around) * away)
         y = centerY + (sin(around) * away)
 
-
         thetime = DateTime(everyday) + Dates.Hour(12)
-        age = first(df[df[:, :isotime] .== thetime, :age])
+        age   = first(df[df[:, :isotime] .== thetime, :age])
+        positionangle = first(df[df[:, :isotime] .== thetime, :posangle])
 
         # draw moon and dayname
-        gsave()
-        translate(x, y)
-        rotate(around + pi / 2) # rotate and then get text baseline
+        @layer begin
+            translate(x, y)
+            rotate(around + pi / 2) # rotate and then get text baseline
 
-        moon(O, moonsize, age / 29.530589, -pi / 2, "lightyellow", darkblue)
+            moon(O, moonsize, age, positionangle)
 
-        fontface("EurostileLTStd")
-        sethue("white")
-        fontsize(7)
-        # dayname
-        textcentred(Dates.dayname(everyday), 0, - (moonsize + 6))
-        grestore()
+            # dayname
+            fontface("EurostileLT")
+            sethue(foregroundwhite)
+            fontsize(7)
+            text(Dates.dayname(everyday), 0, - (moonsize + 6), halign=:center)
+        end
+
+        sethue(foregroundwhite)
+        fontface("EurostileLT-Bold")
 
         # day number isn't rotated!
-        gsave()
-        a = atan(y, x) - 0.01 # adjust for optical
-        x1, y1 = (away - (moonsize * 1.85)) * cos(a), (away - (moonsize * 1.85)) * sin(a)
-        translate(x1, y1)
-        fontface("EurostileLTStd-Bold")
-        sethue("white")
-        fontsize(11)
-        textcentred(string(d[3])) # day number
-        grestore()
+        @layer begin
+            a = atan(y, x) - 0.01 # adjust for optical
+            x1, y1 = (away - (moonsize * 1.85)) * cos(a), (away - (moonsize * 1.85)) * sin(a)
+            translate(x1, y1)
+            fontsize(11)
+            text(string(d[3]), halign=:center) # day number
+        end
 
         # month text needs special handling
         if d[3] == 1
             fontsize(20)
-            fontface("ChunkFive")
             m = string(titlecase(Dates.monthname(everyday)))
             te = textextents(m)
-            sethue("white")
-            twwidth = te[3]
             r = away + (moonsize * 2.2)
             θ = atan(y, x)
             pt = polar(r, θ)
+
+            # you could use this for circular text curve
+            # textcurve(m, θ, r, O)
+
+            # but this follows the spiral better
+
             for ch in m
                 te = textextents(string(ch))
                 twwidth = te[1] + te[3] + te[5]
@@ -151,74 +158,80 @@ function spiral_calendar(theyear, currentwidth, currentheight)
                 end
                 r -= 0.5
             end
+
         end
         theta += chord / away
     end
 
     # decoration
     setline(4)
-    move(0, 0)
+    move(O)
     rect(-(currentwidth / 2) + 6, -(currentheight / 2) + 6, currentwidth - 12, currentheight - 12, :stroke)
     setline(1)
     rect(-(currentwidth / 2) + 12, -(currentheight / 2) + 12, currentwidth - 24, currentheight - 24, :stroke)
 
-    gsave()
-    for i in 1:4
-        gsave()
-        # large title
-        translate(-(currentwidth / 2) + 40, -(currentwidth / 2) + 110)
-        fontsize(81)
-        fontface("EurostileLT-Bold")
-        text(string(currentyear))
+    @layer begin
+        for i in 1:4
+            @layer begin
+                # large title
+                translate(-(currentwidth / 2) + 40, -(currentwidth / 2) + 110)
+                fontsize(81)
+                fontface("EurostileLT-Bold")
+                text(string(currentyear))
 
-        fontsize(21)
-        fontface("EurostileLT-Bold")
-        text("moon phase calendar", 5, 22)
+                fontsize(21)
+                fontface("EurostileLT-Bold")
+                text("moon phase calendar", 5, 22)
 
-        # orbital logo
-        setline(.3)
-        shift = textextents("moon phase calendar")[5]
-        translate(shift / 2, 60)
+                # orbital logo
+                setline(.3)
+                shift = textextents("moon phase calendar")[5]
+                translate(shift / 2, 60)
 
-        # orbit elliptical
-        gsave()
-        Luxor.scale(1, 0.2)
-        circle(O, 45, :stroke)
-        grestore()
+                # elliptical orbit
+                @layer begin
+                    Luxor.scale(1, 0.2)
+                    circle(O, 45, :stroke)
+                end
 
-        # planet and moon
-        gsave()
-        setline(.7)
-        sethue(darkblue)
-        circle(0, -3, 7, :stroke)
-        grestore()
-        circle(0, -3, 7, :fill) # planet
-        circle(45, 0, 3, :fill) # moon
-        grestore()
-        rotate(pi / 2)
+                # planet and moon
+                @layer begin
+                    setline(.7)
+                    circle(45, 0, 3, :fill) # moon
+                    sethue(backgroundblue)
+                    circle(0, -3, 7, :stroke)
+                    sethue(foregroundwhite)
+                    circle(0, -3, 7, :fill) # planet
+                end
+            end
+            rotate(pi / 2)
+        end # layer
+
+        # and finally an email address
+        setopacity(0.8)
+        fontsize(2)
+        sethue(35 / 255, 35 / 255, 150 / 255)
+        translate(0, -70 + (currentheight + 100) / 2)
+        text("cormullion@mac.com", halign=:center)
     end
-    grestore()
-
-    # and finally an email address
-    setopacity(0.8)
-    fontsize(4)
-    sethue(35 / 255, 35 / 255, 150 / 255)
-    translate(0, -70 + (currentheight + 100) / 2)
-    textcentred("cormullion@mac.com")
 end
 
 # start here
 
-theyear = 2021
+# download data from NASA
+# data_url = "https://svs.gsfc.nasa.gov/vis/a000000/a004900/a004955/mooninfo_2022.json"
+# Downloads.download(data_url, "nasa-moon-2022-data.json")
 
-currentwidth = 1500
-currentheight = 1500
+const theyear = 2022
+const currentwidth = 1500
+const currentheight = 1500
 
+const backgroundblue = RGB(16 / 255, 16 / 255, 80 / 255)
+const foregroundwhite = RGB(1, 1, 0.87)
 
-darkblue = RGB(20 / 255, 20 / 255, 80 / 255)
 Drawing(currentwidth + 100, currentheight + 100, "/tmp/$(theyear)-moon-phase-calendar.pdf")
 origin()
-background(darkblue)
+background(backgroundblue)
 spiral_calendar(theyear, currentwidth, currentheight)
 finish()
 preview()
